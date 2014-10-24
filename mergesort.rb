04bff20ab39952b8c3a6de31e824fb870909eb97
Cate Uselton <cathode.use@gmail.com>
2014-10-24 15:50:14 -0700

    Merge sort

---
+++ mergesort.rb
@@ -0,0 +1,26 @@
+def merge_sort(ar)
+  if ar.length > 1
+    middle = ar.length/2
+    res1 = merge_sort(ar[0..middle-1])
+    res2 = merge_sort(ar[middle..-1])
+    merge(res1, res2)
+  else
+    ar
+  end
+end
+
+def merge(ar1, ar2)
+  sorted_array = []
+  if ar1.empty?
+    ar2
+  elsif ar2.empty?
+    ar1
+  else
+    if ar1[0] <= ar2[0]
+      sorted_array.push(ar1.shift)
+    else
+      sorted_array.push(ar2.shift)
+    end
+    sorted_array += merge(ar1, ar2)
+  end
+end
